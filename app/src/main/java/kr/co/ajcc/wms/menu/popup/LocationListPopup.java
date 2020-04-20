21ca6e65980e82289baafde0624e9e43ca877ecb
Jung <bantis0771@gmail.com>
2020-04-20 17:14:23 +0900

    DATE : 2020-04-20 AUTHOR : 정제영 COMMENT : 입고등록 spinner 추가 작업

--- app/src/main/java/kr/co/ajcc/wms/menu/popup/LocationListPopup.java
+++ app/src/main/java/kr/co/ajcc/wms/menu/popup/LocationListPopup.java
@@ -13,17 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import kr.co.ajcc.wms.R;
 import kr.co.ajcc.wms.menu.common.Util;
+import kr.co.ajcc.wms.menu.spinner.SpinnerAdapter;
+import kr.co.ajcc.wms.menu.spinner.SpinnerPopupAdapter;
 
 public class LocationListPopup {
     Activity mActivity;
@@ -31,6 +36,9 @@ public class LocationListPopup {
     Dialog dialog;
     ArrayList<String> mList;
     Handler mHandler;
+
+    Spinner mSpinner;
+    int mSpinnerSelect = 0;
 
     public LocationListPopup(Activity activity, ArrayList<String> list, int title, Handler handler){
         mActivity = activity;
@@ -73,8 +81,18 @@ public class LocationListPopup {
         ImageView iv_title = dialog.findViewById(R.id.iv_title);
         iv_title.setBackgroundResource(title);
 
-        TextView tv_warehouse =  dialog.findViewById(R.id.tv_warehouse);
-        tv_warehouse.setText("선택된 창고");
+        List<String> list = new ArrayList<String>();
+        list.add("창고 1");
+        list.add("창고 2");
+        list.add("창고 3");
+        list.add("창고 4");
+        list.add("창고 5");
+
+        mSpinner =  dialog.findViewById(R.id.spinner);
+        SpinnerPopupAdapter spinnerAdapter = new SpinnerPopupAdapter(activity, list, mSpinner);
+        mSpinner.setAdapter(spinnerAdapter);
+        mSpinner.setOnItemSelectedListener(onItemSelectedListener);
+        mSpinner.setSelection(mSpinnerSelect);
 
         ListView listView = dialog.findViewById(R.id.list);
 
@@ -87,13 +105,6 @@ public class LocationListPopup {
 
         ListAdapter adapter = new ListAdapter();
         listView.setAdapter(adapter);
-
-        dialog.findViewById(R.id.bt_select).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Util.Toast(mActivity, "선택");
-            }
-        });
 
         dialog.findViewById(R.id.bt_search).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,6 +122,27 @@ public class LocationListPopup {
 
         dialog.show();
     }
+
+    AdapterView.OnItemSelectedListener onItemSelectedListener = new AdapterView.OnItemSelectedListener(){
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            //최초에 setOnItemSelectedListener 하면 이벤트가 들어오기 때문에
+            //onResume에서 mSpinnerSelect에 현재 선택된 position을 넣고 여기서 비교
+            if(mSpinnerSelect == position)return;
+
+            mSpinnerSelect = position;
+
+            String item = (String) mSpinner.getSelectedItem();
+            Util.Toast(mActivity, item+" 선택");
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
+    };
 
     class ListAdapter extends BaseAdapter {
         LayoutInflater mInflater;
--- app/src/main/java/kr/co/ajcc/wms/menu/registration/RegistrationActivity.java
+++ app/src/main/java/kr/co/ajcc/wms/menu/registration/RegistrationActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,7 @@ import kr.co.ajcc.wms.menu.common.Util;
 import kr.co.ajcc.wms.menu.custom.BaseCompatActivity;
 import kr.co.ajcc.wms.menu.popup.LocationListPopup;
 import kr.co.ajcc.wms.menu.popup.OneBtnPopup;
+import kr.co.ajcc.wms.menu.spinner.SpinnerAdapter;
 
 /**
  * 입고등록
@@ -25,6 +28,9 @@ public class RegistrationActivity extends BaseCompatActivity {
 
     LocationListPopup mLocationListPopup;
     OneBtnPopup mOneBtnPopup;
+
+    Spinner mSpinner;
+    int mSpinnerSelect = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +44,42 @@ public class RegistrationActivity extends BaseCompatActivity {
         findViewById(R.id.bt_back).setOnClickListener(onClickListener);
         findViewById(R.id.bt_change).setOnClickListener(onClickListener);
         findViewById(R.id.bt_search).setOnClickListener(onClickListener);
-        findViewById(R.id.bt_select).setOnClickListener(onClickListener);
         findViewById(R.id.bt_next).setOnClickListener(onClickListener);
+
+        List<String> list = new ArrayList<String>();
+        list.add("창고 1");
+        list.add("창고 2");
+        list.add("창고 3");
+        list.add("창고 4");
+        list.add("창고 5");
+
+        mSpinner =  findViewById(R.id.spinner);
+        SpinnerAdapter spinnerAdapter = new SpinnerAdapter(mContext, list, mSpinner);
+        mSpinner.setAdapter(spinnerAdapter);
+        mSpinner.setOnItemSelectedListener(onItemSelectedListener);
+        mSpinner.setSelection(mSpinnerSelect);
     }
+
+    AdapterView.OnItemSelectedListener onItemSelectedListener = new AdapterView.OnItemSelectedListener(){
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            //최초에 setOnItemSelectedListener 하면 이벤트가 들어오기 때문에
+            //onResume에서 mSpinnerSelect에 현재 선택된 position을 넣고 여기서 비교
+            if(mSpinnerSelect == position)return;
+
+            mSpinnerSelect = position;
+
+            String item = (String) mSpinner.getSelectedItem();
+            Util.Toast(mContext, item+" 선택");
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
+    };
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
@@ -71,8 +110,6 @@ public class RegistrationActivity extends BaseCompatActivity {
                             }
                         }
                     });
-                    break;
-                case R.id.bt_select:
                     break;
                 case R.id.bt_next:
                     mOneBtnPopup = new OneBtnPopup(RegistrationActivity.this, "품질 검사 완료 후 입고등록을 하십시오.", R.drawable.popup_title_alert, new Handler() {
